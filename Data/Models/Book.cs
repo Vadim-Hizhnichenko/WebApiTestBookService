467101b70c211befd7e90f31c9c6a4aebf7b8d8e
Vadim Hizhnichenko <yankeevadim@gmail.com>
2021-10-12 23:58:05 +0300

    First test commit

---
+++ Data/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title {get; set;}
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int? Rate { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string UrlPath { get; set; }
+
+    }
+}
